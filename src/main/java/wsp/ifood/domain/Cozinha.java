fde05a0722e2634718e1f331ed356a9c5b2daf8a
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2023-05-19 01:01:57 -0300

    implementando o domain de cozinha

---
+++ src/main/java/wsp/ifood/domain/Cozinha.java
@@ -0,0 +1,24 @@
+package wsp.ifood.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	@Column(nullable = false)
+	private String nome;
+	
+}
