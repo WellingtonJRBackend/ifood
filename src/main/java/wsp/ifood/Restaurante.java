d7929826e0639c4435f1c74e0b0853804e7ef189
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2023-05-19 01:13:28 -0300

    implementando o domain de restaurante

---
+++ src/main/java/wsp/ifood/Restaurante.java
@@ -0,0 +1,26 @@
+package wsp.ifood;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID ID;
+	@Column(nullable = false)
+	private String nome;
+	@Column(name = "taxa_frente", nullable = false)
+	private BigDecimal taxaFrente;
+}
