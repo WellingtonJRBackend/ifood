ad5902c1c59824ca14492cfc36b893a3398f26a2
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2023-05-23 00:01:55 -0300

    Implementando o domain de estado

---
+++ src/main/java/wsp/ifood/domain/Estado.java
@@ -0,0 +1,23 @@
+package wsp.ifood.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Estado {
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id ;
+	@Column(nullable = false, length = 100)
+	private String nome;
+	@ManyToOne
+	private Cidade cidade;
+}
