d0435ffde962319c91b6d642f9d10ebe84c16cd8
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2023-06-26 23:04:31 -0300

    relacionamento unidirecional

--- src/main/java/wsp/ifood/domain/Usuario.java
+++ src/main/java/wsp/ifood/domain/Usuario.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -19,12 +22,22 @@ public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@Column(nullable = false)
 	private String nome;
+	
 	@Column(nullable = false)
 	private String email;
+	
 	@Column(nullable = false)
 	private String senha;
+	
 	@CreationTimestamp
 	private LocalDateTime dataCadastro;
+	
+	@ManyToMany
+	@JoinTable(name = "usuario_grupo",
+	           joinColumns = @JoinColumn(name = "usuario_id"),
+	           inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+	private List<Grupo> grupos = new ArrayList<>();
 }
