3877db919b0afbdfc8f1a2791273e4988d1b249a
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2023-06-26 22:57:17 -0300

    implementação do domain de grupo

---
+++ src/main/java/wsp/ifood/domain/Grupo.java
@@ -0,0 +1,29 @@
+package wsp.ifood.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+public class Grupo {
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private UUID id;
+	@Column(nullable = false)
+	private String nome;
+	@ManyToMany
+	@JoinTable(name = "grupo_permissao",
+	           joinColumns = @JoinColumn(name = "grupo_id"),
+	           inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+	private List<Permissao> permissaos = new ArrayList<>();
+}
