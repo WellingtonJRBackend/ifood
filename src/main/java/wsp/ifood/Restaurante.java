e5f8b19843c8489cede42659b966eb56f0b36aff
WellingtonJRBackend <wellington.2001quirino@gmail.com>
2023-08-09 00:44:01 -0300

    alterações na classe de restaurante

--- src/main/java/wsp/ifood/Restaurante.java
+++ src/main/java/wsp/ifood/Restaurante.java
@@ -1,18 +1,20 @@
 package wsp.ifood;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import wsp.ifood.domain.Cozinha;
-import wsp.ifood.domain.Produto;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import wsp.ifood.domain.*;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.UUID;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,16 +24,42 @@ import java.util.UUID;
 public class Restaurante {
 	@EqualsAndHashCode.Include
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private UUID ID;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	@Column(nullable = false)
 	private String nome;
-	@Column(name = "taxa_frente", nullable = false)
-	private BigDecimal taxaFrente;
+	
+	@Column(name = "taxa_frete", nullable = false)
+	private BigDecimal taxaFrete;
+	
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
-	@JsonIgnore
+	
+	@Embedded
+	private Endereco endereco;
+	
+	@CreationTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private OffsetDateTime dataCadastro;
+	
+	@UpdateTimestamp
+	@Column(nullable = false, columnDefinition = "datetime")
+	private OffsetDateTime dataAtualizacao;
+	
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+	           joinColumns = @JoinColumn(name = "restaurante_id"),
+	           inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private Set<FormaPagamento> formasPagamento = new HashSet<>();
+	
+	@ManyToMany
+	@JoinTable(name = "restaurante_usuario_responsavel",
+	           joinColumns = @JoinColumn(name = "restaurante_id"),
+	           inverseJoinColumns = @JoinColumn(name = "usuario_id"))
+	private Set<Usuario> responsaveis = new HashSet<>();
+	
 	@OneToMany(mappedBy = "restaurante")
 	private List<Produto> produtos = new ArrayList<>();
 }
